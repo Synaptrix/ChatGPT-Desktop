86d08f7230da6d6c0070698f0c43a67dc57d3b2b
ayang <75017711+bilibili-ayang@users.noreply.github.com>
2023-03-20 15:17:51 +0800

    perf: optimized some details (#31)

--- src/components/Session/index.vue
+++ src/components/Session/index.vue
@@ -30,31 +30,26 @@ watchEffect(() => {
     ref="sessionElement"
     class="session relative flex-1 cursor-default overflow-auto"
   >
-    <div
-      class="bg-gray/60 z-999 sticky top-2 m-auto h-3 w-80 cursor-move rounded-md opacity-0 hover:opacity-100"
-      data-tauri-drag-region
-    ></div>
     <template v-if="sessionDataList.length">
       <div
-        :id="`session-${item.id}`"
+        :id="`session-data-${item.id}`"
         class="flex items-start gap-4 py-2 px-4"
         v-for="item in sessionDataList"
         :key="item.time"
       >
+        <!-- TODO: 优化 css -->
         <Avatar class="w-12!" :value="item.is_ask ? uuid : currentRole?.name" />
-        <div
-          class="group relative flex flex-1 flex-col gap-3.5 py-[11.5px] leading-6"
-          :key="item.id"
-        >
+        <div class="group relative flex flex-1 flex-col py-3" :key="item.id">
           <div
             class="text-5 absolute top-2 right-2 opacity-0 transition group-hover:opacity-100"
           >
             <IconImage
               class="cursor-pointer"
-              @click="saveImage(`session-${item.id}`)"
+              @click="saveImage(`session-data-${item.id}`)"
             />
           </div>
           <div
+            class="session-item flex flex-col gap-4 leading-6"
             v-html="marked.render(item.message.content)"
             :class="!item.message.content && 'blink-block'"
           ></div>
@@ -74,14 +69,20 @@ watchEffect(() => {
     @apply absolute h-6 w-1 bg-[var(--color-text-2)] content-none;
   }
 
-  ::v-deep(.markdown-it-code-copy) {
-    width: 20px;
-    height: 20px;
+  ::v-deep(pre) {
+    margin: 0;
+    code {
+      @apply rounded-md;
+    }
+    + .markdown-it-code-copy {
+      width: 20px;
+      height: 20px;
 
-    border: 0;
-    outline: 0;
-    background: url('@/assets/image/copy.svg') no-repeat center;
-    background-size: contain;
+      border: 0;
+      outline: 0;
+      background: url('@/assets/image/copy.svg') no-repeat center;
+      background-size: contain;
+    }
   }
 
   @keyframes blink {
