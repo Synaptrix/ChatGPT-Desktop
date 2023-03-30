ba2c0e936c4db3389d5f7975f8720673aa490cae
ayang <473033518@qq.com>
2023-03-30 11:36:47 +0800

    fix: fix the bug that mac hide cannot be opened from the launch pad

--- src/utils/tauri.ts
+++ src/utils/tauri.ts
@@ -1,5 +1,8 @@
 import { invoke } from '@tauri-apps/api/tauri'
 import { downloadDir } from '@tauri-apps/api/path'
+import { type } from '@tauri-apps/api/os'
+import { appWindow } from '@tauri-apps/api/window'
+import { hide, show } from '@tauri-apps/api/app'
 
 /**
  * 打开文件所在位置
@@ -9,4 +12,34 @@ export const openFilePath = async (file: string) => {
   await invoke('show_in_folder', {
     path: (await downloadDir()) + file
   })
+}
+
+/**
+ * 显示窗口
+ */
+export const showWindow = async () => {
+  const platform = await type()
+
+  appWindow.unminimize()
+
+  if (platform === 'Darwin') {
+    show()
+  } else {
+    appWindow.show()
+  }
+
+  appWindow.setFocus()
+}
+
+/**
+ * 隐藏窗口
+ */
+export const hideWindow = async () => {
+  const platform = await type()
+
+  if (platform === 'Darwin') {
+    hide()
+  } else {
+    appWindow.hide()
+  }
 }
